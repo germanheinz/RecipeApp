3e45490b9b6af8d0005a6845c265197fd9f8614c
germanheinz <heinz.german@gmail.com>
2020-12-03 15:42:36 +0100

    Recipe Model and Recipe Detail HTML

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+
+  recipes: Recipe[] = [
+    new Recipe('TACO', 'MEXICAN FOOD', 'https://media.globalcitizen.org/4d/08/4d080f57-5b38-4358-ab44-ce1796f482d6/beyond_beef.png')
+  ];
 
   constructor() { }
 
